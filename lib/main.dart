cb1f7296a2f5c22368f9f7e2568d833a910dddef
Dan Maklen <danmaklen@gmail.com>
2022-01-31 13:22:21 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Dan Maklen App',
+      home: Scaffold(
+        body: Text('Hello World'),
+      ),
+    );
+  }
+}
