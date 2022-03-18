e331e0db33c211820567c7ce9ec7e827333d2fe2
Dan Maklen <danmaklen@gmail.com>
2022-03-18 03:18:07 +0200

    Github Action

--- Alwan.Infra/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Alwan.Infra.Constructs;
-using Amazon.CDK;
-
-App app = new App();
-Config config = Config.FromContext(app);
-Stack _ = new SinglePageApplicationStack(app, config);
-app.Synth();
