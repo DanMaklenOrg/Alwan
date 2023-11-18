ceed290db63753e8b706c35eec5bb70122d49fcb
Dan Maklen <danmaklen@gmail.com>
2023-11-19 00:57:50 +0200

    client

--- lib/service_provider.dart
+++ lib/service_provider.dart
@@ -6,10 +6,16 @@ import 'package:get_it/get_it.dart';
 var serviceProvider = GetIt.instance;
 
 final class ServiceCollection {
-  static void registerServices(){
-    serviceProvider.registerSingleton<Config>(const Config.fromEnv());
-
-    final dio = serviceProvider.registerSingleton<Dio>(Dio());
+  static void registerServices() {
+    final config = serviceProvider.registerSingleton<Config>(const Config.fromEnv());
+    final dio = serviceProvider.registerSingleton<Dio>(_resolveDio(config));
     serviceProvider.registerSingleton(ApiClient(dio));
+  }
+
+  static Dio _resolveDio(Config config) {
+    var dio = Dio();
+    dio.options.baseUrl = config.baseUrl;
+    dio.interceptors.add(LogInterceptor(responseBody: true));
+    return dio;
   }
 }
