705b6abcc333b41c4bdc6db427f30c437983cd11
Dan Maklen <danmaklen@gmail.com>
2024-01-07 17:35:25 +0200

    Delete old stuff

--- lib/pika/models/domain.dart
+++
@@ -1,9 +0,0 @@
-import 'package:alwan/pika/models/achievement.dart';
-
-class Domain {
-  const Domain({required this.id, required this.name, required this.achievements});
-
-  final String id;
-  final String name;
-  final List<Achievement> achievements;
-}
