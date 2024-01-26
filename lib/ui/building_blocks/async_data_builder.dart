96e1248b4fde803076ca2cadd14d61f22bcf5d12
Dan Maklen <danmaklen@gmail.com>
2024-01-26 19:35:23 +0200

    Save Stats

--- lib/ui/building_blocks/async_data_builder.dart
+++ lib/ui/building_blocks/async_data_builder.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 typedef DataBuilder<T> = Widget Function(BuildContext context, T data);
 typedef DataFetcher<T> = Future<T> Function();
