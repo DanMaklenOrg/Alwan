d914669189251a3ce1cb6765643651a8d0fe9a9c
Dan Maklen <danmaklen@gmail.com>
2024-08-10 15:08:40 +0300

    Update DTOs & Use Json Serializer

--- lib/pika/pika_container.dart
+++ lib/pika/pika_container.dart
@@ -30,15 +30,12 @@ final class PikaContainerBuilder {
 
   final List<DomainDto> _domains = [];
   final List<StatDto> _statsDto = [];
-  final List<TagDto> _tagsDto = [];
   final List<ClassDto> _classesDto = [];
   final List<EntityDto> _entitiesDto = [];
   final List<ProjectDto> _projectsDto = [];
 
   void loadDomain(DomainDto domain, {bool listDomain = true}) {
-    final domainList = _flattenSubDomains(domain);
-    _statsDto.addAll(domainList.expand((d) => d.stats));
-    _tagsDto.addAll(domainList.expand((d) => d.tags));
+    final domainList = [domain];
     _classesDto.addAll(domainList.expand((d) => d.classes));
     _entitiesDto.addAll(domainList.expand((d) => d.entities));
     _projectsDto.addAll(domainList.expand((d) => d.projects));
@@ -48,7 +45,6 @@ final class PikaContainerBuilder {
   PikaContainer build() {
     DtoConverter converter = DtoConverter(container);
     container.stats.addResourceList(_statsDto.map(converter.fromStatDto));
-    container.tags.addResourceList(_tagsDto.map(converter.fromTagDto));
     container.classes.addResourceList(_classesDto.map(converter.fromClassDto));
 
     container.entities.addResourceList(_entitiesDto.map(converter.fromEntityDto));
@@ -58,12 +54,5 @@ final class PikaContainerBuilder {
     container.domains.addAll(_domains.map(converter.fromDomainDto));
     container.domains.sort((a, b) => a.name.compareTo(b.name));
     return container;
-  }
-
-  Iterable<DomainDto> _flattenSubDomains(DomainDto dto) sync* {
-    yield dto;
-    for (var subDomain in dto.subDomains) {
-      yield* _flattenSubDomains(subDomain);
-    }
   }
 }
