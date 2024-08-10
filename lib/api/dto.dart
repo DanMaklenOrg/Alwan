9707123e4c98cc1efddd62b7c47b338f871278b2
Dan Maklen <danmaklen@gmail.com>
2024-08-10 15:22:45 +0300

    Update DTOs & Use Json Serializer

--- lib/api/dto.dart
+++ lib/api/dto.dart
@@ -2,33 +2,36 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'dto.g.dart';
 
+@JsonSerializable()
 final class SignInRequestDto {
-  const SignInRequestDto({
-    required this.username,
-    required this.password,
-  });
+  const SignInRequestDto({required this.username, required this.password});
 
-  Map<String, dynamic> toJson() {
-    return {
-      'username': username,
-      'password': password,
-    };
-  }
+  factory SignInRequestDto.fromJson(Map<String, dynamic> json) => _$SignInRequestDtoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SignInRequestDtoToJson(this);
 
   final String username;
   final String password;
 }
 
+@JsonSerializable()
 final class SignInResponseDto {
-  SignInResponseDto.fromJson(Map<String, dynamic> json) : token = json['token'] as String;
+  SignInResponseDto({required this.token});
+
+  factory SignInResponseDto.fromJson(Map<String, dynamic> json) => _$SignInResponseDtoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SignInResponseDtoToJson(this);
 
   final String token;
 }
 
+@JsonSerializable()
 final class DomainSummaryDto {
-  DomainSummaryDto.fromJson(Map<String, dynamic> json)
-      : id = json['id'] as String,
-        name = json['name'] as String;
+  DomainSummaryDto({required this.id, required this.name});
+
+  factory DomainSummaryDto.fromJson(Map<String, dynamic> json) => _$DomainSummaryDtoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DomainSummaryDtoToJson(this);
 
   final String id;
   final String name;
@@ -39,6 +42,7 @@ final class DomainDto {
   DomainDto({required this.id, required this.name, required this.projects, required this.classes, required this.entities});
 
   factory DomainDto.fromJson(Map<String, dynamic> json) => _$DomainDtoFromJson(json);
+
   Map<String, dynamic> toJson() => _$DomainDtoToJson(this);
 
   final String id;
@@ -53,6 +57,7 @@ final class ProjectDto {
   ProjectDto({required this.title, required this.objectives});
 
   factory ProjectDto.fromJson(Map<String, dynamic> json) => _$ProjectDtoFromJson(json);
+
   Map<String, dynamic> toJson() => _$ProjectDtoToJson(this);
 
   final String title;
@@ -64,6 +69,7 @@ final class ObjectiveDto {
   ObjectiveDto({required this.title, required this.requirements});
 
   factory ObjectiveDto.fromJson(Map<String, dynamic> json) => _$ObjectiveDtoFromJson(json);
+
   Map<String, dynamic> toJson() => _$ObjectiveDtoToJson(this);
 
   final String title;
@@ -75,6 +81,7 @@ final class ObjectiveRequirementDto {
   ObjectiveRequirementDto({required this.$class, required this.stat, required this.min});
 
   factory ObjectiveRequirementDto.fromJson(Map<String, dynamic> json) => _$ObjectiveRequirementDtoFromJson(json);
+
   Map<String, dynamic> toJson() => _$ObjectiveRequirementDtoToJson(this);
 
   final String $class;
@@ -87,6 +94,7 @@ final class ClassDto {
   ClassDto({required this.id, required this.stats});
 
   factory ClassDto.fromJson(Map<String, dynamic> json) => _$ClassDtoFromJson(json);
+
   Map<String, dynamic> toJson() => _$ClassDtoToJson(this);
 
   final String id;
@@ -98,6 +106,7 @@ final class EntityDto {
   EntityDto({required this.id, required this.name, required this.$class, required this.stats});
 
   factory EntityDto.fromJson(Map<String, dynamic> json) => _$EntityDtoFromJson(json);
+
   Map<String, dynamic> toJson() => _$EntityDtoToJson(this);
 
   final String id;
@@ -111,6 +120,7 @@ final class StatDto {
   StatDto({required this.id, required this.name, required this.type, required this.min, required this.max, required this.enumValues});
 
   factory StatDto.fromJson(Map<String, dynamic> json) => _$StatDtoFromJson(json);
+
   Map<String, dynamic> toJson() => _$StatDtoToJson(this);
 
   final String id;
@@ -121,19 +131,11 @@ final class StatDto {
   final List<String>? enumValues;
 }
 
+@JsonEnum(fieldRename: FieldRename.pascal)
 enum StatTypeEnumDto {
   boolean,
   integerRange,
   orderedEnum;
-
-  factory StatTypeEnumDto.fromString(String str) {
-    return switch (str) {
-      "Boolean" => StatTypeEnumDto.boolean,
-      "IntegerRange" => StatTypeEnumDto.integerRange,
-      "OrderedEnum" => StatTypeEnumDto.orderedEnum,
-      _ => throw RangeError("Unable to convert $str to StatTypeEnumDto")
-    };
-  }
 }
 
 @JsonSerializable()
@@ -141,10 +143,10 @@ final class UserStatsDto {
   UserStatsDto({required this.entityStats});
 
   factory UserStatsDto.fromJson(Map<String, dynamic> json) => _$UserStatsDtoFromJson(json);
+
   Map<String, dynamic> toJson() => _$UserStatsDtoToJson(this);
 
   final List<UserEntityStatDto> entityStats;
-
 }
 
 @JsonSerializable()
@@ -152,6 +154,7 @@ final class UserEntityStatDto {
   UserEntityStatDto({required this.entityId, required this.statId, required this.value});
 
   factory UserEntityStatDto.fromJson(Map<String, dynamic> json) => _$UserEntityStatDtoFromJson(json);
+
   Map<String, dynamic> toJson() => _$UserEntityStatDtoToJson(this);
 
   final String entityId;
